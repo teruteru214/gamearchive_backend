7b61c6d44be0876b8b499020f4e45bf704f8021b
teruteru <tutituti3080@gmail.com>
2023-07-22 16:46:24 +0900

    Refactor: Userモデルのバリテーションを一部修正

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   validates :nickname,    presence: true, length: { maximum: 40 }
-  validates :uid,         uniqueness: true
-  validates :avatar_key,  uniqueness: true
+  validates :uid,         presence:true, uniqueness: true
   validates :introduction, length: { maximum: 160 }
   validates :visibility,  inclusion: { in: %w(private public), message: "%{value} is not a valid visibility" }
+
+  def self.find_or_create_user(user_info)
+    user = User.find_by(uid: user_info[:uid])
+
+    if user
+      return { user: user, is_new: false }
+    else
+      new_user = User.create!(uid: user_info[:uid], nickname: "User_#{user_info[:uid]}", visibility: "private")
+      return { user: new_user, is_new: true }
+    end
+  end
 end

e4ae94a7b9c03e0e78612d82bfe4637a3d04546c
teruteru <tutituti3080@gmail.com>
2023-07-23 11:47:56 +0900

    refactor: API_DOMAINの環境変数をfly側でセットするので削除

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["API_DOMAIN"] || "gamearchiveapp.com"
+    origins ENV["API_DOMAIN"] || ""
 
     resource '*',
       headers: :any,

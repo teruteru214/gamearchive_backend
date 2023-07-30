a5ebc4d419ac5537beba04a5524c56a3d9adfb1a
teruteru <tutituti3080@gmail.com>
2023-07-30 11:37:47 +0900

    refactor: ゲーム検索のルーティングを追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       # api test action
       resources :hello, only: %w[index]
-      resource :image, only: %w[create show]
       resource :authentication, only: %w[create]
+      resource :image, only: %w[create show]
       resource :profile, only: %w[show update]
+      post '/search', to: 'search#search'
     end
   end
 end

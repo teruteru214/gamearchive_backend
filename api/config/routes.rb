b91f9f8cd4ee3c977fd1f4b1c3b2c1474b593916
teruteru <tutituti3080@gmail.com>
2023-08-01 20:51:03 +0900

    feat: ルーティングの作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, format: "json" do
     namespace :v1 do
-      # api test action
       resources :hello, only: %w[index]
       resource :authentication, only: %w[create]
       resource :image, only: %w[create show]
       resource :profile, only: %w[show update]
       post '/search', to: 'search#search'
+      resources :games, only: [:create]
+      resources :game_statuses, only: [:create]
     end
   end
 end

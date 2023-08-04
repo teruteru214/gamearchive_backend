4db7f76c88c2448caa53bfe8f78f6bc2b3b1cc2e
teruteru <tutituti3080@gmail.com>
2023-08-04 17:51:00 +0900

    feat:　ゲームの情報を渡すシリアライザーの作成

---
+++ api/app/serializers/game_card_serializer.rb
@@ -0,0 +1,20 @@
+class GameCardSerializer
+  include JSONAPI::Serializer
+  attributes :id, :title, :cover, :rating, :url
+
+  attribute :game_status do |game|
+    game.game_status.attributes.slice('id', 'status')
+  end
+
+  attribute :game_genres do |game|
+    game.genres.map do |genre|
+      { id: genre.id, name: genre.name }
+    end
+  end
+
+  attribute :game_platforms do |game|
+    game.platforms.map do |platform|
+      { id: platform.id, name: platform.name }
+    end
+  end
+end

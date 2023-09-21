03af02460b5fc35db5bfc434da18bf7a75268f82
teruteru <tutituti3080@gmail.com>
2023-09-21 12:31:49 +0900

    Feat: line_settingsテーブルに関する制約を追加

---
+++ api/app/models/line_setting.rb
@@ -0,0 +1,8 @@
+class LineSetting < ApplicationRecord
+  belongs_to :user
+
+  validates :line_user_id, presence: true, uniqueness: true
+  validates :stacked_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :favorite_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :line_notification, inclusion: { in: [true, false] }
+end

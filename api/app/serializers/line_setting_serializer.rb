ea1aede48be0953766e2ea4d5c71223b107799c6
teruteru <tutituti3080@gmail.com>
2023-09-24 17:46:06 +0900

    Fix: line_user_idを外部に漏らさないためにシリアライザーの項目から外す

--- api/app/serializers/line_setting_serializer.rb
+++ api/app/serializers/line_setting_serializer.rb
@@ -1,4 +1,4 @@
 class LineSettingSerializer
   include JSONAPI::Serializer
-  attributes :line_user_id, :line_notification, :stacked_notification_interval, :favorite_notification_interval
+  attributes :id, :line_notification, :stacked_notification_interval, :favorite_notification_interval
 end

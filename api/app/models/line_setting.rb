1012b2da5f9b72b876b09819307f0053cf0303e9
teruteru <tutituti3080@gmail.com>
2023-09-26 10:47:02 +0900

    refactor: 通知感覚の数字が1~60までの数字しか入らないように

--- api/app/models/line_setting.rb
+++ api/app/models/line_setting.rb
@@ -2,7 +2,7 @@ class LineSetting < ApplicationRecord
   belongs_to :user
 
   validates :line_user_id, presence: true, uniqueness: true
-  validates :stacked_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :favorite_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :stacked_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 60 }
+  validates :favorite_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 60 }
   validates :line_notification, inclusion: { in: [true, false] }
 end

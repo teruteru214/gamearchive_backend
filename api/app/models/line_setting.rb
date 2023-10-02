b25640c01699310ea63219dff47ad16643e9da03
teruteru <tutituti3080@gmail.com>
2023-10-02 20:15:14 +0900

    Remove: favorite_notification_intervalに関するコードを削除

--- api/app/models/line_setting.rb
+++ api/app/models/line_setting.rb
@@ -2,7 +2,5 @@ class LineSetting < ApplicationRecord
   belongs_to :user
 
   validates :line_user_id, presence: true, uniqueness: true
-  validates :stacked_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 60 }
-  validates :favorite_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 60 }
-  validates :line_notification, inclusion: { in: [true, false] }
+  validates :stacked_notification_interval, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 60 }
 end

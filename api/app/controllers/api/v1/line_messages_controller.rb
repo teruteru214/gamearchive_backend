497b7342c136ed83edd2b025192a28cc330d261f
teruteru <tutituti3080@gmail.com>
2023-10-03 21:39:04 +0900

    Fix: お気に入りのゲームを持っていない公開ユーザーが選ばれないように修正

--- api/app/controllers/api/v1/line_messages_controller.rb
+++ api/app/controllers/api/v1/line_messages_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::LineMessagesController < Api::V1::BaseController
 
   def handle_text_message(event)
     if event.message["text"] == "他ユーザーのお気に入りゲームを教えて"
-      user = User.where(visibility: "public").order("RANDOM()").first
+      user = User.joins(:favorites).where(visibility: "public").distinct.order("RANDOM()").first
       game = user.favorited_games.order("RANDOM()").first if user
 
       if user && game

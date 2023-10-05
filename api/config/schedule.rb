4dd77a1f38bb027e82120a85b8a0a4399260801e
teruteru <tutituti3080@gmail.com>
2023-10-06 08:33:24 +0900

    Remove: wheneverのcron処理が動作しなかったので削除

--- api/config/schedule.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/environment")
-
-set :output, "log/cron.log"
-
-every 1.day, at: '12:00 pm' do
-  runner "LineSetting.notify_users"
-end
--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# cronをバックグラウンドで起動
-service cron start
-
-# Railsサーバーを起動
-bundle exec rails s -p 3000 -b '0.0.0.0'

c05927144ac00858cdfe5dc7e77ad25f15e90478
teruteru <tutituti3080@gmail.com>
2023-10-11 11:29:22 +0900

    Feat: LINE通知で特定のメッセージ以外の対応

--- api/app/controllers/api/v1/line_messages_controller.rb
+++ api/app/controllers/api/v1/line_messages_controller.rb
@@ -25,18 +25,24 @@ class Api::V1::LineMessagesController < Api::V1::BaseController
   end
 
   def handle_text_message(event)
+    line_client = LineClient.new
+
     if event.message["text"] == "他ユーザーのお気に入りゲームを教えて"
       subquery = User.joins(:favorites).where(visibility: "public").distinct.select(:id)
       user = User.where(id: subquery).order("RANDOM()").first
-
       game = user.favorited_games.order("RANDOM()").first if user
 
       if user && game
         line_message_service = LineFavoriteGameService.new(user, game)
         flex_contents = line_message_service.generate_flex_message
-        line_client = LineClient.new
-        line_client.push_flex_message(user.line_setting.line_user_id, "#{user.nickname}さんのお気に入りのゲーム", flex_contents)
+        line_client.push_flex_message(user.line_setting.line_user_id, "#{user.nickname}さんのお気に入りゲーム", flex_contents)
       end
+    else
+      message = {
+        type: "text",
+        text: "メッセージありがとうございます！申し訳ございませんが、現在のリクエストには個別に対応することができません。何かご質問やご要望がありましたら、こちらのXアカウント [@teruteru1237](https://twitter.com/teruteru1237) のDMでご連絡ください。🎮"
+      }
+      client.reply_message(event['replyToken'], message)
     end
   end
 end
